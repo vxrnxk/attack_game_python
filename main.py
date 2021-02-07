7ec7b3893265686ab48352025b748a989c45e96b
Francisco V <vxrnxk@gmail.com>
2021-02-07 13:25:20 -0300

    starting characters

---
+++ main.py
@@ -0,0 +1,17 @@
+class Character:
+    def __init__(self, name, category, level, power, health):
+        self.name = name
+        self.category = category
+        self.level = level
+        self.power = power
+        self.health = health
+
+    def __str__(self):
+        return f'{self.name} ({self.category}) - PWR: {self.power} | HLT: {self.health} | LVL: {self.level}'
+
+
+knight = Character('Player K 01', 'Knight', 1, 10, 5)
+mage = Character('Player M 01', 'Mage', 1, 5, 10)
+
+print(knight)
+print(mage)
