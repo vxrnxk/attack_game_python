81c95752b7dbf6981fbcdcf6239931807398e67f
Francisco V <vxrnxk@gmail.com>
2021-02-07 13:34:28 -0300

    basic attack

--- main.py
+++ main.py
@@ -9,9 +9,14 @@ class Character:
     def __str__(self):
         return f'{self.name} ({self.category}) - PWR: {self.power} | HLT: {self.health} | LVL: {self.level}'
 
+    def attack(self):
+        print(f'{self.name} attacked...')
+
 
 knight = Character('Player K 01', 'Knight', 1, 10, 5)
 mage = Character('Player M 01', 'Mage', 1, 5, 10)
 
 print(knight)
 print(mage)
+
+mage.attack()
