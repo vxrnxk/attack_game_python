68788499da864aa374a1ef2f3f0111d5a4f64dbe
Francisco V <vxrnxk@gmail.com>
2021-02-12 20:04:41 -0300

    set default player name if not typed

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 class Character:
     def __init__(self, name, category, level, power, health):
         self.name = name
@@ -6,8 +9,15 @@ class Character:
         self.power = power
         self.health = health
 
+        if name:
+            self.name = name
+        else:
+            self.name = category + str(randint(1,99))
+
+
     def __str__(self):
         return f'{self.name} ({self.category}) - PWR: {self.power} | HLT: {self.health} | LVL: {self.level}'
+
 
     def attack(self):
         print(f'{self.name} attacked...')
@@ -15,8 +25,10 @@ class Character:
 
 knight = Character('Player K 01', 'Knight', 1, 10, 5)
 mage = Character('Player M 01', 'Mage', 1, 5, 10)
+archer = Character(None, 'Archer', 1, 8, 6)
 
 print(knight)
 print(mage)
+print(archer)
 
 mage.attack()
